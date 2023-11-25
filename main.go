cecb9d8d4d5d071620c9c8f836026566961e8859
Dave Vasilevsky <dave@vasilevsky.ca>
2023-11-24 23:41:58 -0500

    print name

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"log"
 	"os"
+	"syscall"
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
 type fsys struct {
@@ -24,6 +26,32 @@ func (f *fsys) Attr(ctx context.Context, a *fuse.Attr) error {
 	return nil
 }
 
+func (f *fsys) Lookup(ctx context.Context, name string) (fs.Node, error) {
+	hash := plumbing.NewHash(name)
+	commit, err := f.repo.CommitObject(hash)
+	if err == plumbing.ErrObjectNotFound {
+		return nil, syscall.ENOENT
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &commitFile{commit}, nil
+}
+
+type commitFile struct {
+	commit *object.Commit
+}
+
+func (f *commitFile) Attr(ctx context.Context, a *fuse.Attr) error {
+	a.Mode = 0o444
+	a.Size = uint64(len(f.commit.Author.Name))
+	return nil
+}
+
+func (f *commitFile) ReadAll(ctx context.Context) ([]byte, error) {
+	return []byte(f.commit.Author.Name), nil
+}
+
 func main() {
 	if len(os.Args) != 3 {
 		log.Fatalln("Usage: gitfuse REPO MOUNTPOINT")
@@ -36,18 +64,11 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	commitId := "a5417c87d3db74901a10eae3f74af230811d2886"
-	hash := plumbing.NewHash(commitId)
-	commit, err := repo.CommitObject(hash)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	println(commit.Author.Name)
-
 	conn, err := fuse.Mount(mountpoint)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	defer conn.Close()
 
 	fs.Serve(conn, &fsys{repo})
 }
