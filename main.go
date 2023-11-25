4368f2d3ef67dffaed8d0fe879df1d9eb9e4669f
Dave Vasilevsky <dave@vasilevsky.ca>
2023-11-24 23:31:22 -0500

    basic git

--- main.go
+++ main.go
@@ -7,17 +7,19 @@ import (
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 )
 
-type fsys string
-
-func (f fsys) Root() (fs.Node, error) {
-	return rootNode(f), nil
+type fsys struct {
+	repo *git.Repository
 }
 
-type rootNode string
+func (f *fsys) Root() (fs.Node, error) {
+	return f, nil
+}
 
-func (n rootNode) Attr(ctx context.Context, a *fuse.Attr) error {
+func (f *fsys) Attr(ctx context.Context, a *fuse.Attr) error {
 	a.Mode = os.ModeDir | 0o555
 	return nil
 }
@@ -26,13 +28,26 @@ func main() {
 	if len(os.Args) != 3 {
 		log.Fatalln("Usage: gitfuse REPO MOUNTPOINT")
 	}
-	repo := os.Args[1]
+	repopath := os.Args[1]
 	mountpoint := os.Args[2]
+
+	repo, err := git.PlainOpen(repopath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	commitId := "a5417c87d3db74901a10eae3f74af230811d2886"
+	hash := plumbing.NewHash(commitId)
+	commit, err := repo.CommitObject(hash)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	println(commit.Author.Name)
 
 	conn, err := fuse.Mount(mountpoint)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	fs.Serve(conn, fsys(repo))
+	fs.Serve(conn, &fsys{repo})
 }
