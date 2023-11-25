a5417c87d3db74901a10eae3f74af230811d2886
Dave Vasilevsky <dave@vasilevsky.ca>
2023-11-24 23:09:34 -0500

    fuse

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/vasi/gitfuse
 
 go 1.18
+
+require bazil.org/fuse v0.0.0-20230120002735-62a210ff1fd5
+
+require golang.org/x/sys v0.4.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"os"
 
+	"bazil.org/fuse"
+	"bazil.org/fuse/fs"
+)
+
+type fsys string
+
+func (f fsys) Root() (fs.Node, error) {
+	return rootNode(f), nil
+}
+
+type rootNode string
+
+func (n rootNode) Attr(ctx context.Context, a *fuse.Attr) error {
+	a.Mode = os.ModeDir | 0o555
+	return nil
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatalln("Usage: gitfuse REPO MOUNTPOINT")
+	}
+	repo := os.Args[1]
+	mountpoint := os.Args[2]
+
+	conn, err := fuse.Mount(mountpoint)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fs.Serve(conn, fsys(repo))
 }
